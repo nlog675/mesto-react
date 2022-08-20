a7bec8ad53a65c7326463ac6f086c344e381785c
nlog <nlog675@gmail.com>
2022-08-20 18:17:53 +0300

    add cards promise & getting cards info from server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import avatar from '../images/avatar.jpg';
 import { api } from '../utils/api';
 
 function Main(props) {
-    const [userName, setUserName] = React.useState()
-    const [userDescription, setUserDescription] = React.useState()
-    const [userAvatar, setUserAvatar] = React.useState()
+    const [userName, setUserName] = React.useState();
+    const [userDescription, setUserDescription] = React.useState();
+    const [userAvatar, setUserAvatar] = React.useState();
+    const [cards, setCards] = React.useState([]);
 
     React.useEffect(() => {
-        api.getProfile()
-            .then((user) => {
+        Promise.all([api.getProfile(), api.getCard()])
+            .then(([user, data]) => {
                 setUserName(user.name);
                 setUserDescription(user.about);
                 setUserAvatar(user.avatar);
+                setCards(data)
             })
             .catch((err) => console.log(err))
     })
-
 
     return(
         <main className="content">
@@ -35,7 +35,21 @@ function Main(props) {
             
             <section className="elements">
                 <ul className="elements__list">
-    
+                    {cards.map((data) => {
+                        return(
+                            <li key={data._id} className="elements__item">
+                                <button type="button" className="elements__card-delete"></button>
+                                <div style={{ backgroundImage: `url(${data.link})` }} className="elements__item-image" />
+                                <div className="elements__card-info">
+                                    <h2 className="elements__card-heading">{data.name}</h2>
+                                    <div>
+                                        <button type="button" className="elements__card-like"></button>
+                                        <div className="elements__card-like-counter">{data.likes.length}</div>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
                 </ul>
             </section>
         </main>
