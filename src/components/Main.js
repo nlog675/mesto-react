ab1fd7357385d8bf960c00f5d236a7442cd81ec3
nlog <nlog675@gmail.com>
2022-08-20 13:25:53 +0300

    add user promise & getting user info rom server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import avatar from '../images/avatar.jpg';
+import { api } from '../utils/api';
 
-function Main({onEditProfile, onAddPlace, onEditAvatar}) {
+function Main(props) {
+    const [userName, setUserName] = React.useState()
+    const [userDescription, setUserDescription] = React.useState()
+    const [userAvatar, setUserAvatar] = React.useState()
+
+    React.useEffect(() => {
+        api.getProfile()
+            .then((user) => {
+                setUserName(user.name);
+                setUserDescription(user.about);
+                setUserAvatar(user.avatar);
+            })
+            .catch((err) => console.log(err))
+    })
 
 
     return(
         <main className="content">
             <section className="profile">
-                <button onClick={onEditAvatar} className="profile__button-change-avatar"></button>
-                <img src={avatar} alt="Аватар автора" className="profile__avatar" />
+                <button onClick={props.onEditAvatar} className="profile__button-change-avatar"></button>
+                <div style={{ backgroundImage: `url(${userAvatar})` }} className="profile__avatar" />
                 <div className="profile__info">
                     <div className="profile__container">
-                        <h1 className="profile__name">Жак-Ив Кусто</h1>
-                        <button onClick={onEditProfile} type="button" className="profile__edit-button"></button>
+                        <h1 className="profile__name">{userName}</h1>
+                        <button onClick={props.onEditProfile} type="button" className="profile__edit-button"></button>
                     </div>
-                    <h2 className="profile__description">Исследователь океана</h2>
+                    <h2 className="profile__description">{userDescription}</h2>
                 </div>
-                <button onClick={onAddPlace} type="button" className="profile__add-button"></button>
+                <button onClick={props.onAddPlace} type="button" className="profile__add-button"></button>
             </section>
             
             <section className="elements">
